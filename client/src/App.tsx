dc3cebd989fddaa8480b23b92b1b030446877198
soumya tiwari <soumyatiwari7866@gmail.com>
2026-07-02 18:08:51 +0530

    fix:resolve redirectingHomepage component code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@
 import Signup from './components/Signup'
 
 import './App.css'
-import {Navigate, BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Navigate, BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
 import { PageNotFound } from './components/PageNotFound'
 import { GoogleOAuthProvider } from '@react-oauth/google'
@@ -13,27 +13,57 @@ import Alerts from './components/Alerts'
 
 function App() {
 
-  const GoogleAuthWrapper=()=>{
-    return(
-    <GoogleOAuthProvider clientId='1034823575253-7muj29cpa2tt48kob2brue9hafp75ide.apps.googleusercontent.com'>
-      <Signup/>
-    </GoogleOAuthProvider>
-    )
-  }
+  const isAuthenticated = () => {
+    const userInfo = localStorage.getItem('user-info');
+    if (!userInfo) return false;
+
+    try {
+      const parsed = JSON.parse(userInfo);
+      
+      return !!parsed.token;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  console.log("isAuthenticated:",isAuthenticated());
+
+  const clientID = '1034823575253-7muj29cpa2tt48kob2brue9hafp75ide.apps.googleusercontent.com';
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to={`/Signup`}/>}></Route>
-        <Route path='/signup' element={<GoogleAuthWrapper/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/homepage' element={<Homepage/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/alerts' element={<Alerts/>}></Route>
-        <Route path='/features' element={<Features/>}></Route>
-        <Route path='/*' element={<PageNotFound/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={clientID}>
+      <BrowserRouter>
+        <Routes>
+          
+          <Route
+            path='/'
+            element={isAuthenticated() ? <Navigate to="/homepage" replace /> : <Navigate to="/signup" replace />}
+          />
+
+          
+          <Route
+            path='/signup'
+            element={isAuthenticated()  ? <Navigate to="/homepage" replace /> : <Signup />}
+          />
+          <Route
+            path='/login'
+            element={isAuthenticated()  ? <Navigate to="/homepage" replace /> : <Login />}
+          />
+
+         
+          <Route
+            path='/homepage'
+            element={isAuthenticated()  ? <Homepage /> : <Navigate to="/signup" replace />}
+          />
+
+          
+          <Route path='/about' element={<About />} />
+          <Route path='/alerts' element={<Alerts />} />
+          <Route path='/features' element={<Features />} />
+          <Route path='/*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   )
 }
 
